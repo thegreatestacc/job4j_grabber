9d932a8648c480810bd27c9bdb7636c5684f1a90
thegreatestacc <thegreatestemailname@gmail.com>
2020-12-17 20:43:47 +0300

    data to string

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -3,9 +3,15 @@ package ru.job4j.html;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SqlRuParse {
+    private static String url = "https://www.sql.ru/forum/job-offers";
+
     public static void main(String[] args) throws Exception{
-        var doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        var doc = Jsoup.connect(url).get();
         var row = doc.select(".postslisttopic");
         for (Element e : row) {
             var href = e.child(0);
@@ -14,5 +20,21 @@ public class SqlRuParse {
             System.out.println(href.text());
             System.out.println(date.text());
         }
+    }
+
+    public static List<String> dataToString(String url) {
+        var result = new ArrayList<String>();
+        try {
+            var doc = Jsoup.connect(url).get();
+            var row = doc.select(".postslisttopic");
+            for (Element e : row) {
+                var href = e.child(0);
+                var date = href.parent().parent().child(5);
+                result.add(date.text());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
