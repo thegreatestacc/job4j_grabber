99d7fd6011f8210ea2f775a60269ae438252d848
thegreatestacc <thegreatestemailname@gmail.com>
2021-01-22 18:59:19 +0300

    details + data

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,9 +4,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class SqlRuParse {
@@ -22,8 +21,8 @@ public class SqlRuParse {
 //            System.out.println(href.text());
 
 //            new DateUtils().dateConverter(date.text());
-            System.out.println(downloadDetails());
         }
+        downloadDetails();
     }
 
     public static List<String> dataToString(String url) {
@@ -56,16 +55,34 @@ public class SqlRuParse {
         return result;
     }
 
-    public static List<String> downloadDetails() throws IOException {
-        var result = new ArrayList<String>();
+    public static HashMap<Object, Object> downloadDetails() throws IOException {
+        var desc = new ArrayList<String>();
+        var data = new ArrayList<String>();
+        var result = new HashMap<>();
         url = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
         var doc = Jsoup.connect(url).get();
         var row = doc.select(".msgBody");
+        var footer = doc.select(".msgFooter");
 
-        for (Element e : row) {
-            var href = e.child(0).parent().parent();
-            result.add(href.text());
-        }
+        var info = row.stream()
+                .map(element -> {
+                    var href = element.child(0).parent().parent().child(1);
+                    desc.add(href.text());
+                    return desc.get(0);
+                }).findFirst().get();
+
+        var str = footer.stream()
+                .map(element -> {
+                    var href = element.child(0).parent().parent();
+                    data.add(href.text());
+                    return data.get(0);
+                }).findFirst().get();
+
+        var split = str.split(",");
+
+        result.put(split[0], info);
+        System.out.println(split[0] + "\n" + info);
+
         return result;
     }
 }
